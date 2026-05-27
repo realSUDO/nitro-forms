5f0e07e67213811370d1daf14f8823b8afbab379
Sumit Vishwkarma <justmultiplythinks@gmail.com>
2026-05-27 16:56:54 +0530

    fix: add 'use client' to builder page for dynamic import with ssr:false

--- apps/web/app/(app)/builder/[id]/page.tsx
+++ apps/web/app/(app)/builder/[id]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import dynamic from "next/dynamic";
 
 const FormBuilder = dynamic(() => import("~/components/form-builder").then(m => ({ default: m.FormBuilder })), { ssr: false, loading: () => <div className="flex-1 flex items-center justify-center"><div className="animate-spin w-8 h-8 border-2 border-[#5865f2] border-t-transparent rounded-full" /></div> });
